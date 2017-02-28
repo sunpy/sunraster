adbda4cada5572ac23ecfd41088e0ecc0f659ef2
DanRyanIrish <ryand5@tcd.ie>
2017-02-28 09:59:35 -0500

    Added function calculate_fractional_uncertainty() to iris_tools.py to calculate fractional uncertainty of an array of IRIS data.

--- irispy/iris_tools.py
+++ irispy/iris_tools.py
@@ -60,3 +60,35 @@ def convert_photons_to_DN(data, detector_type):
 
     """
     return DETECTOR_YIELD[detector_type]/DETECTOR_GAIN[detector_type]*data
+
+
+def calculate_intensity_fractional_uncertainty(data, data_unit, detector_type):
+    """Calculates fractional uncertainty of IRIS data.
+
+    Parameters
+    ----------
+    data: array-like
+        IRIS data.
+    data_unit: `str`
+        Unit of data.  Must be either 'DN' or 'photons'.
+    detector_type: `str`
+        Detector/CCD upon which data was recorded.
+        Can take values 'NUV', 'FUV' or 'SJI'.
+
+    Returns
+    -------
+    fractional_uncertainty: array-like
+        Fractional uncertainty of each element in data array.
+        Same shape as data.
+
+    """
+    photons_per_dn = DETECTOR_GAIN[detector_type]/DETECTOR_YIELD[detector_type]
+    if data_unit == "DN":
+        intensity_ph = photons_per_dn*convert_DN_to_photons(data, detector_type)
+    elif data_unit == "photons":
+        intensity_ph = data
+    else:
+        raise ValueError("Data not in recognized units: {0}".format(data_unit))
+    readout_noise_ph = READOUT_NOISE[detector_type]["value"]*photons_per_dn
+    uncertainty_ph = np.sqrt(intensity_ph+readout_noise_ph**2.)
+    return uncertainty_ph/intensity_ph
