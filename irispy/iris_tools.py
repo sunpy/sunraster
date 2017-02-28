8693a348a20414aeb9aaff765aa629e459ddd4b6
DanRyanIrish <ryand5@tcd.ie>
2017-02-28 09:51:31 -0500

    Addded functions to iris_tools.py to convert data between units of data number and photon counts.

---
+++ irispy/iris_tools.py
@@ -0,0 +1,62 @@
+"""IRIS instrument tools."""
+
+import datetime
+import warnings
+import os.path
+
+import numpy as np
+from astropy.io import fits
+import astropy.units as u
+from astropy.units.quantity import Quantity
+from astropy import wcs
+from astropy.modeling import models, fitting
+from astropy.modeling.models import custom_model
+import scipy.io
+from sunpy.time import parse_time
+
+# Define some properties of IRIS detectors.  Source: IRIS instrument
+# paper.
+DETECTOR_GAIN = {"NUV": 18., "FUV": 6., "SJI": 18.}
+DETECTOR_YIELD = {"NUV": 1., "FUV": 1.5, "SJI": 1.}
+READOUT_NOISE = {"NUV": {"value": 1.2, "unit": "DN"}, "FUV": {"value": 3.1, "unit": "DN"},
+                 "SJI": {"value": 1.2, "unit": "DN"}}
+
+
+def convert_DN_to_photons(data, detector_type):
+    """Converts IRIS data number to photon counts depending on which CCD is being used.
+
+    Parameters
+    ----------
+    data: array-like
+        IRIS data in units of DN.
+    detector_type: `str`
+        Detector/CCD upon which data was recorded.
+        Can take values 'NUV', 'FUV' or 'SJI'.
+
+    Returns
+    -------
+    data_photons: array-like
+        IRIS data in units of photon counts.
+
+    """
+    return DETECTOR_GAIN[detector_type]/DETECTOR_YIELD[detector_type]*data
+
+
+def convert_photons_to_DN(data, detector_type):
+    """Converts photon counts to IRIS data number depending on which CCD is being used.
+
+    Parameters
+    ----------
+    data: array-like
+        IRIS data in units of photon counts.
+    detector_type: `str`
+        Detector/CCD upon which data was recorded.
+        Can take values 'NUV', 'FUV' or 'SJI'.
+
+    Returns
+    -------
+    data_dn: array-like
+        IRIS data in units of data number.
+
+    """
+    return DETECTOR_YIELD[detector_type]/DETECTOR_GAIN[detector_type]*data
