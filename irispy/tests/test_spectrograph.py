cfb74468a889b490fe5970eeef9eaca69b35adc1
abit2 <ankit.baruah1@gmail.com>
2017-03-09 23:17:26 +0530

    changed from using data.key() to spectral_windows['name']

--- irispy/tests/test_spectrograph.py
+++ irispy/tests/test_spectrograph.py
@@ -31,19 +31,19 @@ def test_fits_data_comparison(iris_l2_test_raster):
     data2[hdulist[2].data == -200.] = np.nan
     data3[hdulist[3].data == -200.] = np.nan
 
-    spectral_windows = iris_l2_test_raster.data.keys()
+    spectral_windows = iris_l2_test_raster.spectral_windows['name']
 
-    np.testing.assert_allclose(iris_l2_test_raster.data[spectral_windows[2]].data, data1)
-    np.testing.assert_allclose(iris_l2_test_raster.data[spectral_windows[0]].data, data2)
-    np.testing.assert_allclose(iris_l2_test_raster.data[spectral_windows[1]].data, data3)
+    np.testing.assert_allclose(iris_l2_test_raster.data[spectral_windows[1]].data, data2)
+    np.testing.assert_allclose(iris_l2_test_raster.data[spectral_windows[0]].data, data1)
+    np.testing.assert_allclose(iris_l2_test_raster.data[spectral_windows[2]].data, data3)
 
 
 def test_wcs(iris_l2_test_raster):
     wcs_l = iris_l2_test_raster.wcs
     hdulist = fits.open(os.path.join(testpath, 'iris_l2_20170222_153635_3690215148_raster_t000_r00000.fits'))
     for key, value in six.iteritems(wcs_l):
-		for key_, value_ in wcs_l[key].iteritems():
-			assert isinstance(value_, wcs.WCS)
+        for key_, value_ in six.iteritems(wcs_l[key]):
+            assert isinstance(value_, wcs.WCS)
     # spectral_windows = iris_l2_test_raster.data.keys()
 
     # assert all( wcs.WCS(hdulist[2].header).wcs.crpix == wcs_l['spectral'][spectral_windows[2]].wcs.crpix)
