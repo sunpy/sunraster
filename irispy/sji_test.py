8623e3414d1f3b3a52d4b2d1c2ef22f3d550947c
Shelbe Timothy <shelbe@lmsal.com>
2017-07-11 11:17:50 -0700

    test data
    test data and fixes

--- irispy/sji_test.py
+++ irispy/sji_test.py
@@ -1,30 +1,32 @@
 import sji
 import matplotlib.pyplot as plt
 import numpy.ma as ma
+from irispy.data.sample import SJI_CUBE_1330, SJI_CUBE_1400, SJI_CUBE_2796, SJI_CUBE_2832
 
 iris_dir = '/Users/shelbe/sunpy/data/sample_data/'
-fname = [iris_dir + 'iris_l2_20130801_074720_4040000014_SJI_1400_t000.fits',
-              iris_dir + 'iris_l2_20130801_074720_4040000014_SJI_1400_t000.fits',
-              iris_dir + 'iris_l2_20130801_074720_4040000014_SJI_1400_t000.fits']
+
+flist = [SJI_CUBE_1330, SJI_CUBE_1400, SJI_CUBE_2796, SJI_CUBE_2832]
+
+
 
 def singlefile():
-    mc=sji.SJI_fits_to_cube(fname[0])
+    mc=sji.SJI_fits_to_cube([SJI_CUBE_1330])
     mc.peek()
     plt.show()
     print('Single file input Passed')
 
 def listfits():
-    mc=sji.SJI_fits_to_cube(fname)
+    mc=sji.SJI_fits_to_cube([flist])
     print('Multiple file input Passed')
     # Bad Pixel Map (BPM) before dustbuster
-    m1 = ma.masked_inside(mc[0].data, -199, .1)
+    m1 = ma.masked_inside(mc[0].data, -199, 1)
 
     mc_db=sji.dustbuster(mc)
     # Fail condition: If mask finds unchanged values in BPM will return 'numpy.array'
     # Pass condition: If mask finds no unchanged values in BPM will return 'numpy.bool'
     fail_count=0
     for i, map in enumerate(mc_db):
-        m = ma.masked_inside(map.data, -199, .1)
+        m = ma.masked_inside(map.data, -199, 1)
         if type(m.mask)==type(m1.mask):
             print('DUSTBUSTER Failed: ' +str(i))
             fail_count+=1
@@ -32,6 +34,6 @@ def listfits():
         print('DUSTBUSTER Passed')
 
 
-test1=singlefile()
+#test1=singlefile()
 test2=listfits()
 
