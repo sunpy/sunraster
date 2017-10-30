6d3086fa998e11e1559f66a00498adc810cbdc31
DanRyanIrish <ryand5@tcd.ie>
2017-10-30 16:14:40 -0400

    Bug fix in displaying time/period of IRISSpectrogram in repr string.

--- irispy/spectrograph.py
+++ irispy/spectrograph.py
@@ -427,7 +427,11 @@ class IRISSpectrogram(NDCube):
             mask=result.mask, missing_axis=result.missing_axis)
 
     def __repr__(self):
-        if self.missing_axis[::-1][self._extra_coords["time"]["axis"]]:
+        if self._extra_coords["time"]["axis"] is None:
+            axis_missing = True
+        else:
+            axis_missing = self.missing_axis[::-1][self._extra_coords["time"]["axis"]]
+        if axis_missing is True:
             instance_start = instance_end = self._extra_coords["time"]["value"]
         else:
             instance_start = self._extra_coords["time"]["value"][0],
@@ -590,3 +594,4 @@ def _produce_obs_repr_string(meta):
 OBS Description: {obs_desc}
 OBS period: {obs_start} -- {obs_end}""".format(obs_id=obs_info[0], obs_desc=obs_info[1],
                                                obs_start=obs_info[2], obs_end=obs_info[3])
+
