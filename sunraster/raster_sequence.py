1b490f1cf4e9bd2da36345480cb4b89612b64ca1
DanRyanIrish <ryand5@tcd.ie>
2020-03-29 01:34:28 -0400

    Tidy up order of RasterSequence properties.

--- sunraster/raster_sequence.py
+++ sunraster/raster_sequence.py
@@ -55,6 +55,132 @@ class RasterSequence(NDCubeSequence):
     plot_as_SnS = NDCubeSequence.plot_as_cube
 
     @property
+    def spectral(self):
+        return u.Quantity([raster.spectral for raster in self.data])
+
+    @property
+    def time(self):
+        return np.concatenate([raster.time for raster in self.data])
+
+    @property
+    def exposure_time(self):
+        return np.concatenate([raster.exposure_time for raster in self.data])
+
+    @property
+    def lon(self):
+        return u.Quantity([raster.lon for raster in self.data])
+
+    @property
+    def lat(self):
+        return u.Quantity([raster.lat for raster in self.data])
+
+    @property
+    def raster_axes_types(self):
+        # Get array of indices for each axis.
+        axes_indices = [self._raster_axis_index, self._slit_step_axis_index,
+                        self._slit_axis_raster_index, self._spectral_axis_raster_index]
+        # Remove any Nones.  These correspond to missing axes.
+        axes_indices = np.array(list(filter((None).__ne__, axes_indices)))
+        return tuple(self._raster_axes_names[axes_indices])
+
+    @property
+    def SnS_axes_types(self):
+        # Get array of indices for each axis.
+        axes_indices = [self._SnS_axis_index, self._slit_axis_SnS_index,
+                        self._spectral_axis_SnS_index]
+        # Remove any Nones.  These correspond to missing axes.
+        axes_indices = np.array(list(filter((None).__ne__, axes_indices)))
+        return tuple(self._SnS_axes_names[axes_indices])
+
+    @property
+    def slice_as_SnS(self):
+        """
+        Method to slice instance as though data were taken as a sit-and-stare,
+        i.e. slit position and raster number are combined into a single axis.
+        """
+        return _SnSSlicer(self)
+
+    @property
+    def slice_as_raster(self):
+        """
+        Method to slice instance as though data were 4D, i.e. raster number,
+        slit step position, position along slit, wavelength.
+        """
+        return _SequenceSlicer(self)
+
+    def apply_exposure_time_correction(self, undo=False, copy=False, force=False):
+        """
+        Applies or undoes exposure time correction to data and uncertainty and
+        adjusts unit.
+
+        Correction is only applied (undone) if the object's unit doesn't (does)
+        already include inverse time.  This can be overridden so that correction
+        is applied (undone) regardless of unit by setting force=True.
+
+        Parameters
+        ----------
+        undo: `bool`
+            If False, exposure time correction is applied.
+            If True, exposure time correction is removed.
+            Default=False
+
+        copy: `bool`
+            If True a new instance with the converted data values is returned.
+            If False, the current instance is overwritten.
+            Default=False
+
+        force: `bool`
+            If not True, applies (undoes) exposure time correction only if unit
+            doesn't (does) already include inverse time.
+            If True, correction is applied (undone) regardless of unit.  Unit is still
+            adjusted accordingly.
+
+        Returns
+        -------
+        result: `None` or `RasterSequence`
+            If copy=False, the original RasterSequence is modified with the
+            exposure time correction applied (undone).
+            If copy=True, a new RasterSequence is returned with the correction
+            applied (undone).
+        """
+        converted_data_list = []
+        for cube in self.data:
+            converted_data_list.append(cube.apply_exposure_time_correction(undo=undo,
+                                                                           force=force))
+        if copy is True:
+            return RasterSequence(
+                converted_data_list, meta=self.meta, common_axis=self._SnS_axis_index)
+        else:
+            self.data = converted_data_list
+
+    def __str__(self):
+        if self.data[0]._time_name:
+            time_period = (self.data[0].time[0].value, self.data[-1].time[-1].value)
+        else:
+            time_period = None
+        if self.data[0]._longitude_name:
+            lon_range = u.Quantity([self.lon.min(), self.lon.max()])
+        else:
+            lon_range = None
+        if self.data[0]._latitude_name:
+            lat_range = u.Quantity([self.lat.min(), self.lat.max()])
+        else:
+            lat_range = None
+        if self.data[0]._spectral_name:
+            spectral_range = u.Quantity([self.spectral.min(), self.spectral.max()])
+        else:
+            spectral_range = None
+        return (textwrap.dedent(f"""\
+                RasterSequence
+                --------------
+                Time Range: {time_period}
+                Pixel Dimensions (raster scans, slit steps, slit height, spectral): {self.dimensions}
+                Longitude range: {lon_range}
+                Latitude range: {lat_range}
+                Spectral range: {spectral_range}
+                Data unit: {self.data[0].unit}"""))
+
+    @property
     def _raster_axis_index(self):
         return self.sequence_axis
 
@@ -136,132 +262,6 @@ class RasterSequence(NDCubeSequence):
                 raise TypeError(f"Unrecognized output of world_types_to_axes_func: {axes}.\n"
                                 "Must be a length 1 tuple.")
             return axes[0]
-
-    @property
-    def raster_axes_types(self):
-        # Get array of indices for each axis.
-        axes_indices = [self._raster_axis_index, self._slit_step_axis_index,
-                        self._slit_axis_raster_index, self._spectral_axis_raster_index]
-        # Remove any Nones.  These correspond to missing axes.
-        axes_indices = np.array(list(filter((None).__ne__, axes_indices)))
-        return tuple(self._raster_axes_names[axes_indices])
-
-    @property
-    def SnS_axes_types(self):
-        # Get array of indices for each axis.
-        axes_indices = [self._SnS_axis_index, self._slit_axis_SnS_index,
-                        self._spectral_axis_SnS_index]
-        # Remove any Nones.  These correspond to missing axes.
-        axes_indices = np.array(list(filter((None).__ne__, axes_indices)))
-        return tuple(self._SnS_axes_names[axes_indices])
-
-    def __str__(self):
-        if self.data[0]._time_name:
-            time_period = (self.data[0].time[0].value, self.data[-1].time[-1].value)
-        else:
-            time_period = None
-        if self.data[0]._longitude_name:
-            lon_range = u.Quantity([self.lon.min(), self.lon.max()])
-        else:
-            lon_range = None
-        if self.data[0]._latitude_name:
-            lat_range = u.Quantity([self.lat.min(), self.lat.max()])
-        else:
-            lat_range = None
-        if self.data[0]._spectral_name:
-            spectral_range = u.Quantity([self.spectral.min(), self.spectral.max()])
-        else:
-            spectral_range = None
-        return (textwrap.dedent(f"""\
-                RasterSequence
-                --------------
-                Time Range: {time_period}
-                Pixel Dimensions (raster scans, slit steps, slit height, spectral): {self.dimensions}
-                Longitude range: {lon_range}
-                Latitude range: {lat_range}
-                Spectral range: {spectral_range}
-                Data unit: {self.data[0].unit}"""))
-
-    @property
-    def slice_as_SnS(self):
-        """
-        Method to slice instance as though data were taken as a sit-and-stare,
-        i.e. slit position and raster number are combined into a single axis.
-        """
-        return _SnSSlicer(self)
-
-    @property
-    def slice_as_raster(self):
-        """
-        Method to slice instance as though data were 4D, i.e. raster number,
-        slit step position, position along slit, wavelength.
-        """
-        return _SequenceSlicer(self)
-
-    @property
-    def spectral(self):
-        return u.Quantity([raster.spectral for raster in self.data])
-
-    @property
-    def time(self):
-        return np.concatenate([raster.time for raster in self.data])
-
-    @property
-    def exposure_time(self):
-        return np.concatenate([raster.exposure_time for raster in self.data])
-
-    @property
-    def lon(self):
-        return u.Quantity([raster.lon for raster in self.data])
-
-    @property
-    def lat(self):
-        return u.Quantity([raster.lat for raster in self.data])
-
-    def apply_exposure_time_correction(self, undo=False, copy=False, force=False):
-        """
-        Applies or undoes exposure time correction to data and uncertainty and
-        adjusts unit.
-
-        Correction is only applied (undone) if the object's unit doesn't (does)
-        already include inverse time.  This can be overridden so that correction
-        is applied (undone) regardless of unit by setting force=True.
-
-        Parameters
-        ----------
-        undo: `bool`
-            If False, exposure time correction is applied.
-            If True, exposure time correction is removed.
-            Default=False
-
-        copy: `bool`
-            If True a new instance with the converted data values is returned.
-            If False, the current instance is overwritten.
-            Default=False
-
-        force: `bool`
-            If not True, applies (undoes) exposure time correction only if unit
-            doesn't (does) already include inverse time.
-            If True, correction is applied (undone) regardless of unit.  Unit is still
-            adjusted accordingly.
-
-        Returns
-        -------
-        result: `None` or `RasterSequence`
-            If copy=False, the original RasterSequence is modified with the
-            exposure time correction applied (undone).
-            If copy=True, a new RasterSequence is returned with the correction
-            applied (undone).
-        """
-        converted_data_list = []
-        for cube in self.data:
-            converted_data_list.append(cube.apply_exposure_time_correction(undo=undo,
-                                                                           force=force))
-        if copy is True:
-            return RasterSequence(
-                converted_data_list, meta=self.meta, common_axis=self._SnS_axis_index)
-        else:
-            self.data = converted_data_list
 
     def _raster_axes_to_world_types(self, *axes, include_extra_coords=True):
         """
