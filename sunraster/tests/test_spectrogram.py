fc0a4e02725922ebe9396ef49c389d2efb239390
DanRyanIrish <ryand5@tcd.ie>
2020-04-13 10:47:25 -0400

    Codestyle improvements.

--- sunraster/tests/test_spectrogram.py
+++ sunraster/tests/test_spectrogram.py
@@ -10,75 +10,72 @@ from astropy.time import Time, TimeDelta
 from sunraster import SpectrogramCube
 
 # Define a sample wcs object
-h0 = {
+H0 = {
     'CTYPE1': 'WAVE    ', 'CUNIT1': 'Angstrom', 'CDELT1': 0.2, 'CRPIX1': 0, 'CRVAL1': 10,
     'NAXIS1': 3,
     'CTYPE2': 'HPLT-TAN', 'CUNIT2': 'deg', 'CDELT2': 0.5, 'CRPIX2': 2, 'CRVAL2': 0.5, 'NAXIS2': 2,
     'CTYPE3': 'HPLN-TAN', 'CUNIT3': 'deg', 'CDELT3': 0.4, 'CRPIX3': 2, 'CRVAL3': 1, 'NAXIS3': 2,
 }
-wcs0 = WCS(header=h0, naxis=3)
+WCS0 = WCS(header=H0, naxis=3)
 
 SOURCE_DATA_DN = np.array([[[0.563, 1.132, -1.343], [-0.719, 1.441, 1.566]],
                            [[0.563, 1.132, -1.343], [-0.719, 1.441, 1.566]]])
 SOURCE_UNCERTAINTY_DN = np.sqrt(SOURCE_DATA_DN)
 
-time_dim_len = SOURCE_DATA_DN.shape[0]
-single_exposure_time = 2.
-EXPOSURE_TIME = u.Quantity(np.zeros(time_dim_len) + single_exposure_time, unit=u.s)
+TIME_DIM_LEN = SOURCE_DATA_DN.shape[0]
+SINGLES_EXPOSURE_TIME = 2.
+EXPOSURE_TIME = u.Quantity(np.zeros(TIME_DIM_LEN) + SINGLES_EXPOSURE_TIME, unit=u.s)
 
 # Define sample extra coords
-extra_coords0 = [("time", 0,
-                  Time('2017-01-01') + TimeDelta(np.arange(time_dim_len), format='sec')),
+EXTRA_COORDS0 = [("time", 0,
+                  Time('2017-01-01') + TimeDelta(np.arange(TIME_DIM_LEN), format='sec')),
                  ("exposure time", 0, EXPOSURE_TIME)]
-extra_coords1 = [("time", 0,
+EXTRA_COORDS1 = [("time", 0,
                   (Time('2017-01-01') +
-                   TimeDelta(np.arange(time_dim_len, time_dim_len * 2), format='sec'))),
+                   TimeDelta(np.arange(TIME_DIM_LEN, TIME_DIM_LEN * 2), format='sec'))),
                  ("exposure time", 0, EXPOSURE_TIME)]
-
-# Define meta data
-meta_seq = {"a": 0}
 
 # Define SpectrogramCubes in various units.
 spectrogram_DN0 = SpectrogramCube(
-    SOURCE_DATA_DN, wcs0, extra_coords0, u.ct, SOURCE_UNCERTAINTY_DN)
+    SOURCE_DATA_DN, WCS0, EXTRA_COORDS0, u.ct, SOURCE_UNCERTAINTY_DN)
 spectrogram_DN_per_s0 = SpectrogramCube(
-    SOURCE_DATA_DN / single_exposure_time, wcs0, extra_coords0, u.ct / u.s,
-    SOURCE_UNCERTAINTY_DN / single_exposure_time)
+    SOURCE_DATA_DN / SINGLES_EXPOSURE_TIME, WCS0, EXTRA_COORDS0, u.ct / u.s,
+    SOURCE_UNCERTAINTY_DN / SINGLES_EXPOSURE_TIME)
 spectrogram_DN_per_s_per_s0 = SpectrogramCube(
     SOURCE_DATA_DN /
-    single_exposure_time /
-    single_exposure_time,
-    wcs0,
-    extra_coords0,
+    SINGLES_EXPOSURE_TIME /
+    SINGLES_EXPOSURE_TIME,
+    WCS0,
+    EXTRA_COORDS0,
     u.ct /
     u.s /
     u.s,
     SOURCE_UNCERTAINTY_DN /
-    single_exposure_time /
-    single_exposure_time)
+    SINGLES_EXPOSURE_TIME /
+    SINGLES_EXPOSURE_TIME)
 spectrogram_DN_s0 = SpectrogramCube(
-    SOURCE_DATA_DN * single_exposure_time, wcs0, extra_coords0, u.ct * u.s,
-    SOURCE_UNCERTAINTY_DN * single_exposure_time)
+    SOURCE_DATA_DN * SINGLES_EXPOSURE_TIME, WCS0, EXTRA_COORDS0, u.ct * u.s,
+    SOURCE_UNCERTAINTY_DN * SINGLES_EXPOSURE_TIME)
 spectrogram_DN1 = SpectrogramCube(
-    SOURCE_DATA_DN, wcs0, extra_coords1, u.ct, SOURCE_UNCERTAINTY_DN)
+    SOURCE_DATA_DN, WCS0, EXTRA_COORDS1, u.ct, SOURCE_UNCERTAINTY_DN)
 spectrogram_DN_per_s1 = SpectrogramCube(
-    SOURCE_DATA_DN / single_exposure_time, wcs0, extra_coords1, u.ct / u.s,
-    SOURCE_UNCERTAINTY_DN / single_exposure_time)
+    SOURCE_DATA_DN / SINGLES_EXPOSURE_TIME, WCS0, EXTRA_COORDS1, u.ct / u.s,
+    SOURCE_UNCERTAINTY_DN / SINGLES_EXPOSURE_TIME)
 spectrogram_DN_per_s_per_s1 = SpectrogramCube(
     SOURCE_DATA_DN /
-    single_exposure_time /
-    single_exposure_time,
-    wcs0,
-    extra_coords1,
+    SINGLES_EXPOSURE_TIME /
+    SINGLES_EXPOSURE_TIME,
+    WCS0,
+    EXTRA_COORDS1,
     u.ct /
     u.s /
     u.s,
     SOURCE_UNCERTAINTY_DN /
-    single_exposure_time /
-    single_exposure_time)
+    SINGLES_EXPOSURE_TIME /
+    SINGLES_EXPOSURE_TIME)
 spectrogram_DN_s1 = SpectrogramCube(
-    SOURCE_DATA_DN * single_exposure_time, wcs0, extra_coords1, u.ct * u.s,
-    SOURCE_UNCERTAINTY_DN * single_exposure_time)
+    SOURCE_DATA_DN * SINGLES_EXPOSURE_TIME, WCS0, EXTRA_COORDS1, u.ct * u.s,
+    SOURCE_UNCERTAINTY_DN * SINGLES_EXPOSURE_TIME)
 
 
 @pytest.mark.parametrize("input_cube, undo, force, expected_cube", [
