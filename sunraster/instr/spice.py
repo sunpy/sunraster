25230d09280f70a86839b364007e7d74b9900911
DanRyanIrish <ryand5@tcd.ie>
2020-07-24 10:44:34 -0400

    Add more properties to SPICEMeta.

--- sunraster/instr/spice.py
+++ sunraster/instr/spice.py
@@ -6,6 +6,7 @@ import astropy.units as u
 from astropy.coordinates import SkyCoord
 from astropy.time import Time
 from sunpy.coordinates import HeliographicStonyhurst
+from ndcube import NDCollection
 
 from sunraster.meta import Meta, SlitSpectrographMetaABC
 
@@ -14,6 +15,7 @@ __all__ = ["SPICEMeta"]
 
 
 class SPICEMeta(Meta, metaclass=SlitSpectrographMetaABC):
+    # ---------- SPICE-specific convenience methods ----------
     def _get_unit(self, key):
         try:
             return [s.split("]") for s in self.get_comment(key).split("[")[1:]][0][:-1][0]
@@ -21,18 +23,34 @@ class SPICEMeta(Meta, metaclass=SlitSpectrographMetaABC):
             return None
 
     def _construct_quantity(self, key):
-        val = self.get(key, None)
+        val = self.get(key)
         if val:
             val *= u.Unit(self._get_unit(key))
         return val
 
     def _construct_time(self, key):
-        val = self.get(key, None)
+        val = self.get(key)
         scale = self._get_unit(key).lower()
         if val:
             val = Time(val, format="fits", scale=scale)
         return val
 
+    def __str__(self):
+        return textwrap.dedent(f"""\
+                SPICEMeta
+                ---------
+                Observatory:\t\t{self.observatory}
+                Instrument:\t\t{self.instrument}
+                Detector:\t\t{self.detector}
+                Spectral Window:\t{self.spectral_window}
+                Date:\t\t\t{self.date_reference}
+                OBS ID:\t\t\t{self.observing_mode_id}
+                """)
+
+    def __repr__(self):
+        return f"{object.__repr__(self)}\n{str(self)}"
+
+    # ---------- Inherited ABC properties ----------
     @property
     def spectral_window(self):
         spectral_window = self.get("EXTNAME")
@@ -45,19 +63,19 @@ class SPICEMeta(Meta, metaclass=SlitSpectrographMetaABC):
 
     @property
     def detector(self):
-        return self.get("DETECTOR", None)
+        return self.get("DETECTOR")
 
     @property
     def instrument(self):
-        return self.get("INSTRUME", None)
+        return self.get("INSTRUME")
 
     @property
     def observatory(self):
-        return self.get("OBSRVTRY", None)
+        return self.get("OBSRVTRY")
 
     @property
     def processing_level(self):
-        return self.get("LEVEL", None)
+        return self.get("LEVEL")
 
     @property
     def rsun_meters(self):
@@ -69,11 +87,11 @@ class SPICEMeta(Meta, metaclass=SlitSpectrographMetaABC):
 
     @property
     def observing_mode_id(self):
-        return self.get("OBS_ID", None)
+        return self.get("SPIOBSID")
 
     @property
     def observatory_radial_velocity(self):
-        return self.get("OBS_VR", None)
+        return self.get("OBS_VR")
 
     @property
     def distance_to_sun(self):
@@ -105,17 +123,75 @@ class SPICEMeta(Meta, metaclass=SlitSpectrographMetaABC):
                   'frame': HeliographicStonyhurst}
         return SkyCoord(obstime=self.date_reference, **kwargs)
 
-    def __str__(self):
-        return textwrap.dedent(f"""\
-                SPICEMeta
-                ---------
-                Observatory:\t\t{self.observatory}
-                Instrument:\t\t{self.instrument}
-                Detector:\t\t{self.detector}
-                Spectral Window:\t{self.spectral_window}
-                Date:\t\t\t{self.date}
-                OBS ID:\t\t\t{self.obsid}
-                """)
+    @property
+    def version(self):
+        return self.get("VERSION")
 
-    def __repr__(self):
-        return f"{object.__repr__(self)}\n{str(self)}"
+    # ---------- SPICE-specific metadata properties ----------
+    @property
+    def observing_mode_id_solar_orbiter(self):
+        return self.get("OBS_ID")
+
+    @property
+    def darkmap_subtracted_onboard(self):
+        return bool(self.get("DARKMAP"))
+
+    @property
+    def bias_frame_subtracted_onboard(self):
+        return bool(self.get("BLACKLEV"))
+
+    @property
+    def window_type(self):
+        return self.get("WIN_TYPE")
+
+    @property
+    def window_table_id(self):
+        return self.get("WINTABID")
+
+    @property
+    def slit_id(self):
+        return self.get("SLIT_ID")
+
+    @property
+    def slit_width(self):
+        return self._construct_quantity("SLIT_WID")
+
+    @property
+    def dumbbell(self):
+        dumbell_types = ["none", "lower", "upper"]
+        dumbell_idx = self.get("DUMBBELL")
+        return dumbell_types[dumbell_idx]
+
+    @property
+    def solar_B0(self):
+        """Tilt angle of solar north toward spacecraft."""
+        return self._construct_quantity("SOLAR_B0")
+
+    @property
+    def solar_P0(self):
+        """Angle from spacecraft celestial north to solar north."""
+        return self._construct_quantity("SOLAR_P0")
+
+    @property
+    def solar_ep(self):
+        """Angle from spacecraft ecliptic north to solar north angle."""
+        return self._construct_quantity("SOLAR_EP")
+
+    @property
+    def carrington_rotation(self):
+        """Carrington Rotation number of observation."""
+        return self.get("CAR_ROT")
+
+    @property
+    def date_start_earth(self):
+        """Time at which photons reaching SPICE at start time would have reach Earth."""
+        return self._construct_time("DATE_EAR")
+
+    @property
+    def date_start_sun(self):
+        """
+        Time at which photons reaching SPICE at start time would have left Sun.
+
+        The Sun is defined as the center of the Sun assuming photon was not impeded.
+        """
+        return self._construct_time("DATE_SUN")
--- sunraster/tests/test_spice.py
+++ sunraster/tests/test_spice.py
@@ -23,7 +23,20 @@ DATE_START = ('2020-06-02T07:47:58.017', '[UTC] Beginning of data acquisition')
 DATE_END = ('2020-06-02T07:47:58.117', '[UTC] End of data acquisition')
 HGLN_OBS = (35.8382263864, '[deg] S/C Heliographic longitude')
 HGLT_OBS = (4.83881036748, '[deg] S/C Heliographic latitude (B0 angle)')
-
+SPICE_OBSERVING_MODE_ID = (12583744, 'SPICE Observation ID')
+DARKMAP = (0, 'If set, a dark map was subtracted on-board')
+BLACKLEV = (0, 'If set, a bias frame was subtracted on-board')
+WINDOW_TYPE = ('Full Detector Narrow-slit', 'Description of window type')
+WINDOW_TABLE_ID = (255, 'Index in on-board window data table (0-255)')
+SLIT_ID = (2, 'Slit ID (0-3)')
+SLIT_WIDTH = (4, '[arcsec] Slit width')
+DUMBBELL = (0, '0/1/2: not a dumbbell/lower dumbbel/upper dumbb')
+SOLAR_B0 = (4.83881036748, '[deg] Tilt angle of Solar North toward S/C')
+SOLAR_P0 = (1.49702480927, '[deg] S/C Celestial North to Solar North angle')
+SOLAR_EP = (-6.14143491727, '[deg] S/C Ecliptic  North to Solar North angle')
+CARRINGTON_ROTATION_NUMBER = (2231, 'Carrington rotation number')
+DATE_START_EARTH = ('2020-06-02T07:51:52.799', '[UTC] DATE-BEG + EAR_TDEL')
+DATE_START_SUN = ('2020-06-02T07:43:26.686', '[UTC] DATE-BEG - SUN_TIME')
 
 @pytest.fixture
 def spice_fits_header():
@@ -43,6 +56,20 @@ def spice_fits_header():
     hdr["DATE-END"] = DATE_END
     hdr["HGLN_OBS"] = HGLN_OBS
     hdr["HGLT_OBS"] = HGLT_OBS
+    hdr["SPIOBSID"] = SPICE_OBSERVING_MODE_ID
+    hdr["DARKMAP"] = DARKMAP
+    hdr["BLACKLEV"] = BLACKLEV
+    hdr["WIN_TYPE"] = WINDOW_TYPE
+    hdr["WINTABID"] = WINDOW_TABLE_ID
+    hdr["SLIT_ID"] = SLIT_ID
+    hdr["SLIT_WID"] = SLIT_WIDTH
+    hdr["DUMBBELL"] = DUMBBELL
+    hdr["SOLAR_B0"] = SOLAR_B0
+    hdr["SOLAR_P0"] = SOLAR_P0
+    hdr["SOLAR_EP"] = SOLAR_EP
+    hdr["CAR_ROT"] = CARRINGTON_ROTATION_NUMBER
+    hdr["DATE_EAR"] = DATE_START_EARTH
+    hdr["DATE_SUN"] = DATE_START_SUN
     return hdr
 
 
@@ -53,6 +80,7 @@ def spice_meta(spice_fits_header):
 
 def test_meta_spectral_window(spice_meta):
     assert spice_meta.spectral_window == SPECTRAL_WINDOW[0][8:]
+
 
 def test_meta_detector(spice_meta):
     assert spice_meta.detector == DETECTOR[0]
@@ -79,7 +107,7 @@ def test_meta_rsun_angular(spice_meta):
 
 
 def test_meta_observing_mode_id(spice_meta):
-    assert spice_meta.observing_mode_id == OBSERVING_MODE_ID[0]
+    assert spice_meta.observing_mode_id == SPICE_OBSERVING_MODE_ID[0]
 
 
 def test_meta_observatory_radial_velocity(spice_meta):
@@ -112,3 +140,61 @@ def test_meta_observer_coordinate(spice_meta):
     assert spice_meta.observer_coordinate.radius == observer_coordinate.radius
     assert spice_meta.observer_coordinate.obstime == observer_coordinate.obstime
     assert spice_meta.observer_coordinate.frame.name == observer_coordinate.frame.name
+
+
+def test_meta_observing_mode_id_solar_orbiter(spice_meta):
+    assert spice_meta.observing_mode_id_solar_orbiter == OBSERVING_MODE_ID[0]
+
+
+def test_meta_darkmap_subtracted_onboard(spice_meta):
+    assert spice_meta.darkmap_subtracted_onboard == False
+
+
+def test_meta_bias_frame_subtracted_onboard(spice_meta):
+    assert spice_meta.bias_frame_subtracted_onboard == False
+
+
+def test_meta_window_type(spice_meta):
+    assert spice_meta.window_type == WINDOW_TYPE[0]
+
+
+def test_meta_window_table_id(spice_meta):
+    assert spice_meta.window_table_id == WINDOW_TABLE_ID[0]
+
+
+def test_meta_slit_id(spice_meta):
+    assert spice_meta.slit_id == SLIT_ID[0]
+
+
+def test_meta_slit_width(spice_meta):
+    assert spice_meta.slit_width == SLIT_WIDTH[0] * u.arcsec
+
+
+def test_meta_dumbbell(spice_meta):
+    assert spice_meta.dumbbell == "none"
+
+
+def test_meta_solar_B0(spice_meta):
+    assert spice_meta.solar_B0 == SOLAR_B0[0] * u.deg
+
+
+def test_meta_solar_P0(spice_meta):
+    assert spice_meta.solar_P0 == SOLAR_P0[0] * u.deg
+
+
+def test_meta_solar_ep(spice_meta):
+    assert spice_meta.solar_ep == SOLAR_EP[0] * u.deg
+
+
+def test_meta_carrington_rotation(spice_meta):
+    assert spice_meta.carrington_rotation == CARRINGTON_ROTATION_NUMBER[0]
+
+
+def test_meta_date_start_earth(spice_meta):
+    date_start_earth = Time(DATE_START_EARTH[0], format="fits", scale="utc")
+    assert spice_meta.date_start_earth == date_start_earth
+
+
+def test_meta_date_start_sun(spice_meta):
+    date_start_sun = Time(DATE_START_SUN[0], format="fits", scale="utc")
+    assert spice_meta.date_start_sun == date_start_sun
