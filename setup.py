283b0bd70680bf618dbded4e6f3de95bbb4c0632
Nabil Freij <nabil.freij@gmail.com>
2022-11-09 09:46:33 -0800

    311 testing

--- setup.py
+++ setup.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-
+from setuptools import setup  # isort:skip
 import os
 from itertools import chain
 
-from setuptools import setup
-from setuptools.config import read_configuration
+try:
+    # Recommended for setuptools 61.0.0+
+    # (though may disappear in the future)
+    from setuptools.config.setupcfg import read_configuration
+except ImportError:
+    from setuptools.config import read_configuration
 
-##############################################
-# Programmatically generate some extras combos
-##############################################
-extras = read_configuration("setup.cfg")["options"]["extras_require"]
+################################################################################
+# Programmatically generate some extras combos.
+################################################################################
+extras = read_configuration("setup.cfg")['options']['extras_require']
 
 # Dev is everything
-extras["dev"] = list(chain(*extras.values()))
+extras['dev'] = list(chain(*extras.values()))
 
 # All is everything but tests and docs
 exclude_keys = ("tests", "docs", "dev")
 ex_extras = dict(filter(lambda i: i[0] not in exclude_keys, extras.items()))
 # Concatenate all the values together for 'all'
-extras["all"] = list(chain.from_iterable(ex_extras.values()))
+extras['all'] = list(chain.from_iterable(ex_extras.values()))
 
 setup(
     extras_require=extras,
-    use_scm_version={"write_to": os.path.join("sunraster", "_version.py")},
+    use_scm_version={'write_to': os.path.join('sunraster', '_version.py')},
 )
