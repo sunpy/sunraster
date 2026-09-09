915aae3abcd8c727274b68e8aa5616b70f9cc2a4
Stuart Mumford <stuart@cadair.com>
2026-09-09 12:18:47 +0100

    Update version.py

--- sunraster/version.py
+++ sunraster/version.py
@@ -9,14 +9,10 @@ try:
 except Exception:  # NOQA: BLE001
     import warnings
 
-<<<<<<<
-    warnings.warn(f'could not determine {__name__.split(".")[0]} package version; this indicates a broken installation')
-=======
     warnings.warn(
         f"could not determine {__name__.split('.')[0]} package version; this indicates a broken installation",
         stacklevel=2,
     )
->>>>>>>
     del warnings
 
     version = "0.0.0"
